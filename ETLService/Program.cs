da92c4286f711d7bea46e9278df142a96639caf7
ReneUnan <rene.sandoval23908226@estu.unan.edu.ni>
2025-09-29 14:49:58 -0600

    Fix: Cambiar StartServicesApp a asíncrono para evitar bloqueos.

--- ETLService/Program.cs
+++ ETLService/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-new StartServices().StartServicesApp();
+await new StartServices().StartServicesApp();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
