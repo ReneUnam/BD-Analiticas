a8de2b9aa37395f8dcfd9c320a41dd6ad41fa5d2
ReneUnan <rene.sandoval23908226@estu.unan.edu.ni>
2025-09-29 11:19:31 -0600

    Implement CategoryEntity class and update CategoryOperation to save transformed categories

--- Operations/Category/CategoryDIM.cs
+++ Operations/Category/CategoryDIM.cs
@@ -1,7 +1,15 @@
+using APPCORE;
+using BusinessLogic.Connection;
 namespace Category
 {
-    class CategoryDIM
+    class CategoryDIM : EntityClass
     {
-
+        public CategoryDIM()
+        {
+            this.MDataMapper = new BDConnection().BDDestino;
+        }
+        [PrimaryKey(Identity = false)]
+        public int? Id_Categoria { get; set; }
+        public string? Nombre { get; set; }
     }
 }
--- Operations/Category/CategoryOperation.cs
+++ Operations/Category/CategoryOperation.cs
@@ -1,12 +1,23 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
+using Category;
 namespace Operations.Category
 {
     public class CategoryOperation
     {
-        
+        public void Excute()
+        {
+            //EXTRACT
+            List<CategoryEntity> categoryEntities = new CategoryEntity().Get<CategoryEntity>();
+            //TRANSFORM
+            List<CategoryDIM> categoryDIMs = categoryEntities.Select(category => new CategoryDIM
+            {
+                Id_Categoria = category.Id_Categoria,
+                Nombre = category.Nombre,
+            }).ToList();
+            //LOAD
+            foreach (var categoryDim in categoryDIMs)
+            {
+                categoryDim.Save();
+            }
+        }
     }
 }
