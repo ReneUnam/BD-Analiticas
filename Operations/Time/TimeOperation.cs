230dd958f8ef9144f1754bfc6f60090283a2f506
ReneUnan <rene.sandoval23908226@estu.unan.edu.ni>
2025-10-28 18:13:33 -0600

    feat: Agregar el DIM, generator y operation de Time.

---
+++ Operations/Time/TimeOperation.cs
@@ -0,0 +1,27 @@
+namespace Operations.Time 
+{ 
+    public class TimeOperation 
+    { 
+        public void Excute() 
+        {  
+            var beginTime = DateOLAPOperation.GetLastUpdatedate(); 
+            var endTime = DateTime.Now; 
+             
+             //EXTRACT - TRANSFORM 
+            List<TimeDIM> entitys = TimeGenerator.Generar(beginTime, endTime); 
+            
+            //LOAD 
+            foreach (var entity in entitys) 
+            { 
+                if (new TimeDIM { FechaKey = entity.FechaKey } 
+                    .SimpleFind<TimeDIM>() != null) 
+                { 
+                    continue; 
+                } 
+                entity.Save(); 
+            } 
+             
+        } 
+ 
+    } 
+} 
