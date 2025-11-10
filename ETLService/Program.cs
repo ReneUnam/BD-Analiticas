0e6bb556b68b6ecd4490a0bac35310cbe2cb8a5c
ReneUnan <rene.sandoval23908226@estu.unan.edu.ni>
2025-11-10 09:54:43 -0600

    feat: Agregar el submódulo WDevCore e implementar la configuracion y políticas CORS en Program.cs refactor: Eliminar la clase DateOLAPOperation no utilizada.

--- ETLService/Program.cs
+++ ETLService/Program.cs
@@ -2,23 +2,40 @@ using Operations;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
+builder.Services.AddRazorPages();
+
+//CORS (Cross-Origin Resource Sharing)
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
 
 var app = builder.Build();
 await new StartServices().StartServicesApp();
-Console.WriteLine("ETL terminado. Presiona cualquier tecla para iniciar web...");
-//Console.ReadKey();
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.UseCors(policy => policy
+    .AllowAnyOrigin()
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+);
+
+app.MapControllers();
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.MapRazorPages();
 
 app.Run();
 
